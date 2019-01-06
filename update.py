3e9cb2bcdf789511e1b4c4f5575d71e394473a20
bigcat <redapply0815@gmail.com>
2019-01-06 11:07:35 +0900

    update ver1

---
+++ update.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+print('Content-type: text/html; charset=UTF-8\n')
+import cgi, os
+
+files = os.listdir('data')
+listStr=''
+for item in files:
+    listStr= listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+
+form = cgi.FieldStorage()
+if 'id' in form :
+    pageId = form['id'].value
+    description = open('data/'+pageId,'r').read()
+else:
+    pageId = 'Welcome'
+    description = 'Hello, Web'
+
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB1</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+ 
+  <a href="create.py">create</a>
+  <form action='process_create.py' method='post'>
+    <p><input type="text" name='title' placeholder='title' value="{form_default_tilte}"></p>
+    <p><textarea rows="4" name='description' placeholder='description'>{form_default_description}</textarea>
+    <p><input type='submit'></p>
+    </form>
+ </body>
+</html>
+'''.format(title=pageId,desc=description,listStr=listStr,form_default_tilte=pageId,form_default_description=description))
