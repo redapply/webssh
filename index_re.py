3b214f1b816fb3dea9a6b2828d5a2fc73ecdf4f0
bigcat <redapply0815@gmail.com>
2019-01-07 08:04:42 +0900

    test read1

--- index_re.py
+++ index_re.py
@@ -4,8 +4,10 @@ import cgi
 form = cgi.FieldStorage()
 if 'id' in form:
   pageId = form['id'].value
+  desc = open('data/'+pageId,'r').read()
 else:
-  print('Welcome to Web Python')
+  pageId='Welcome to Web Python'
+  desc = 'Hello, Python Web'
 
 
 print('''<!doctype html>
@@ -22,8 +24,7 @@ print('''<!doctype html>
     <li><a href="index.py?id=JavaScript">JavaScript</a></li>
   </ol>
   <h2>{title}</h2>
-  <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-  </p>
+  <p>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId))
+'''.format(title=pageId,desc=desc))
