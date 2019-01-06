64ba964593507dd3ac361ab7a88e0a7182bf3668
bigcat <redapply0815@gmail.com>
2019-01-06 09:59:48 +0900

    22

--- process_create.py
+++ process_create.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 print('Content-type: text/html; charset=UTF-8\n')
+
+import cgi
+form = cgi.FieldStorage()
+title = form['title'].value
+descrtion = form['description'].value
+
+print(title, descrtion)
+
